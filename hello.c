120b7a9b97e4562a9212a609962a4a31c8bb8e97
Traggy41 <Traggy41@users.noreply.github.com>
2019-01-17 11:40:40 -0600

    update 2.9

--- hello.c
+++ hello.c
@@ -13,6 +13,6 @@ int main(int argc, char **argv) {
   printf("John Hosch\n");
   printf("Electrical Engineering\n");
   printf("Tristan Ragland\n");
-  printf("Business Admin\n");
+  printf("Business Administration\n");
   return 0;
 }
